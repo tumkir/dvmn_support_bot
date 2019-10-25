4a1fe1a2a3849a63179d53aff04dccf6bb93b4cd
tumkir <tumkir@gmail.com>
2019-10-25 17:04:49 +0300

    Переделал try/except: перехватываем конкретные ошибки и в дополнение присылаем Traceback. VK бот больше не ломается при невозможности отправить запрос к Dialogflow

--- vk_bot.py
+++ vk_bot.py
@@ -2,20 +2,18 @@ import logging
 import os
 from random import randint
 
-from vk_api import VkApi
+import vk_api
+from dialogflow import detect_intent_texts
 from dotenv import load_dotenv
+from google.auth import exceptions as google_exceptions
 from vk_api.longpoll import VkEventType, VkLongPoll
 
-from dialogflow import detect_intent_texts
 from tg_logging import MyLogsHandler
 
 
 def send_answer(event, vk_api):
     project_id = os.getenv("PROJECT_ID")
-    try:
-        dialogflow_response = detect_intent_texts(event.user_id, event.text, project_id)
-    except Exception:
-        logger.warning("Невозможно обратиться к dialogflow по запросу из ВК")
+    dialogflow_response = detect_intent_texts(event.user_id, event.text, project_id)
     if not dialogflow_response.query_result.intent.is_fallback:
         vk_api.messages.send(
             user_id=event.user_id,
@@ -26,15 +24,21 @@ def send_answer(event, vk_api):
 def main():
     vk_token = os.getenv("VK_TOKEN")
     try:
-        vk_session = VkApi(token=vk_token)
-        vk_session_api = vk_session.get_api()
-        longpoll = VkLongPoll(vk_session)
-        logger.info("ВК бот запущен")
-        for event in longpoll.listen():
-            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+        vk_session = vk_api.VkApi(token=vk_token)
+    except vk_api.exceptions.ApiError:
+        logger.exception('В файле .env неверный токен VK')
+    vk_session_api = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+    logger.info("ВК бот запущен")
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            try:
                 send_answer(event, vk_session_api)
-    except Exception:
-        logger.warning("Невозможно подключиться к ВК")
+            except google_exceptions.DefaultCredentialsError:
+                logger.exception('Не удалось подключиться в dialogflow по запросу из ВК (скорее всего, неверно указан пусть к json-файлу в .env)')
+            except vk_api.exceptions.ApiError:
+                logger.exception("Не удалось отправить сообщение пользователю в ВК")
+            
 
 
 if __name__ == "__main__":
